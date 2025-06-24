5b7483fa3e45b202bfdd71179127ff5c561c4261
Hector Cald <hi.hector20@gmail.com>
2025-06-24 11:17:45 -0400

    corección de errores

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'totalprod-v8'; // Incrementamos la versión
+const CACHE_NAME = 'totalprod-v9'; // Incrementamos la versión
 const ASSETS_TO_CACHE = [
     '/css/login.css',
     '/js/login.js',
